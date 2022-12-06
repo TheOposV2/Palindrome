d5ba330adbc1a6ae0c5bb0e206ee163245c33ba8
TheOposV2 <119788959+TheOposV2@users.noreply.github.com>
2022-12-06 19:44:47 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,9 @@
 public class Main {
-    public static boolean isItPalidrome(String word)
+)
     {
-        int length = word.length(); //counting the number of letters in a word
-        int halfLength; //variable definition
-
-        if (length%2 == 0) halfLength= length/2; // checking the parity condition and determining position of middle chars
-        else halfLength= (length+1)/2; //else statement and determining position of middle chars for odd numbers of letters input
-
-        for (int i = 0; i < halfLength; i++) //definition of for loop which is needed for typing in chars to arrays
-        {
-            if(word.charAt(i) != word.charAt((length-1)-i)) return false; //comparing opposing chars to determinate if it is a palindrome and if is not return false
-
-        }
-        return true; //comparison of two arrays to check if they are equal
+        for (int i = 0; i < (word.length() + 1) / 2; i++) //definition of for loop which is needed for typing in chars to arrays
+            if(word.charAt(i) != word.charAt(word.length() - 1 - i) return false; //comparing opposing chars to determinate if it is a palindrome and if is not return false
+        return true;
     }
 
     public static void main(String args[])
